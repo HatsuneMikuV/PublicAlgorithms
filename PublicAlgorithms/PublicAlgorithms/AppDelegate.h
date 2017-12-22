42c0e42b8c178dcf4dfedae0728747df7ba0f18b
Icarus <15188588180@163.com>
2017-12-22 15:48:50 +0800

    first update

---
+++ PublicAlgorithms/PublicAlgorithms/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PublicAlgorithms
+//
+//  Created by angle on 2017/12/22.
+//  Copyright © 2017年 angle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PublicAlgorithms/PublicAlgorithms/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PublicAlgorithms
+//
+//  Created by angle on 2017/12/22.
+//  Copyright © 2017年 angle. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
